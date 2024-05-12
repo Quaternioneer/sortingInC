f54e9ec0a4f0f4dc86e664b60e87f957f2eb9ceb
Quaternioneer <nat.admin@protonmail.com>
2024-05-11 20:07:39 -0700

    I changed the index from 1 to n to 0 to n-1.

--- straightSort.c
+++ straightSort.c
@@ -3,15 +3,15 @@
 //Straight Insertion from 8.1 Numerical Recipes in C
 
 void piksrt(int n, float arr[])
-//Sorts an array arr[1..n] into ascending numerical order, by straigh insertion. n is input; arr is replaced on output by its sorted rearrangement.
+//Sorts an array arr[1..n-1] into ascending numerical order, by straigh insertion. n is input; arr is replaced on output by its sorted rearrangement.
 {
    int i,j;
    float a;
 
-   for (j=2;j<=n;j++) { 		//Pick out each element in turn.
+   for (j=1;j<n;j++) { 		//Pick out each element in turn.
        a=arr[j];
        i=j-1;
-       while (i > 0 && arr[i] > a) {    //Look for the place to insert it.
+       while (i >= 0 && arr[i] > a) {    //Look for the place to insert it.
            arr[i+1]=arr[i];
            i--;
         }
